ab415fc57f82aa154e4b13b13fd781ac66f23898
Qso <qi@MacPro.local>
2015-11-03 00:24:58 +0800

    new version

--- 1002.c
+++ 1002.c
@@ -1,65 +1,45 @@
 #include <string.h>
 #include <stdio.h>
 
-#define MAX 1001
-
-char* sum(char *x, const char *y) {
-    int i, j, k, tmp, up = 0;
-
-    for (i = strlen(x), j = strlen(y); j > 0; --i, --j){
-        tmp = x[i-1] + y[j-1] - 96 + up;
-
-        if (tmp >= 10) {
-            x[i-1] = tmp + 48 - 10;
+void sum(const char *x, const char *y) {
+    char z[1001];
+    int i, lx = strlen(x) - 1, ly = strlen(y) - 1, up = 0;
+    for (i = 0; lx >= 0 || ly >= 0; ++i, --lx, --ly) {
+        if (lx >= 0 && ly >= 0) {
+            z[i] = x[lx] + y[ly] - '0' + up;
+        } else if (lx >= 0 && ly < 0) {
+            z[i] = x[lx] + up;
+        } else if (lx < 0 && ly >= 0) {
+            z[i] = y[ly] + up;
+        }
+        up = 0;
+        if (z[i]  > '9') {
             up = 1;
-        } else {
-            x[i-1] = tmp + 48;
-            up = 0;
+            z[i] = z[i] - 10;
         }
     }
-
-    while (up == 1 && j == 0 && i >= 0) {
-        if (i == 0) {
-            for (k = strlen(x); k > 0; --k) {
-                x[k] = x[k-1];
-            }
-            x[0] = up + 48;
-            up = 0;
-        } else {
-            if (x[i-1] + up - 48 < 10) {
-                x[i-1] = x[i-1] + up;
-                up = 0;
-            } else {
-                x[i-1] = '0';
-            }
-            --i;
-        }
+    if (up == 1) {
+        printf("%d", up);
     }
-
-    return x;
+    while (i--) {
+        printf("%c", z[i]);
+    }
+    printf("\n");
 }
 
 int main() {
-    int l, m;
-    char x[MAX], y[MAX];
+    int l, m = 1;
+    char x[1001], y[1001];
 
-    while(scanf("%d", &l) != EOF) {
-        m = 1;
-        while (m <= l) {
-            scanf("%s %s", x, y);
-            unsigned long i = strlen(x);
-            unsigned long j = strlen(y);
+    scanf("%d", &l);
+    while (l--) {
+        scanf("%s %s", x, y);
 
-            printf("Case %d\n%s + %s = ", m++, x, y);
-            if (i >= j) {
-                printf("%s\n", sum(x, y));
-            } else {
-                printf("%s\n", sum(y, x));
-            }
-            if (m <= l) {
-                printf("\n");
-            }
+        printf("Case %d\n%s + %s = ", m++, x, y);
+        sum(x, y);
+        if (l != 1) {
+            printf("\n");
         }
-    };
+    }
     return 0;
 }
---
+++ 1002_k.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char a[1000], b[1000], c[1001];
+    int i, j = 1, p = 0, n, n1, n2;
+    scanf("%d", &n);
+    while(n) {
+        scanf("%s %s", a, b);
+        printf("Case %d:\n", j);
+        printf("%s + %s = ", a, b);
+        n1 = strlen(a) - 1;
+        n2 = strlen(b) - 1;
+        for(i = 0; n1 >= 0 || n2 >= 0; i++, n1--, n2--) {
+            if(n1 >= 0 && n2 >= 0) {
+                c[i] = a[n1] + b[n2] - '0' + p;
+            }
+            if(n1 >= 0 && n2 < 0) {
+                c[i] = a[n1] + p;
+            }
+            if(n1 < 0 && n2 >= 0) {
+                c[i] = b[n2] + p;
+            }
+            p = 0;
+            if(c[i] > '9') {
+                c[i] = c[i] - 10;
+                p = 1;
+            }
+        }
+        if(p == 1) {
+            printf("%d", p);
+        }
+        while(i--) {
+            printf("%c", c[i]);
+        }
+        j++;
+        if(n != 1) {
+            printf("\n\n");
+        } else {
+            printf("\n");
+        }
+        n--;
+    }
+}
